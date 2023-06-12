cbc7eb1b99d14ecd82a4ee225ee60fcf70b842ed
Andre Rousselet <andre098@hotmail.com>
2023-06-12 16:29:12 -0300

    feat(routes): add get and post methods to /users route

--- src/server.js
+++ src/server.js
@@ -1,7 +1,23 @@
 import http from "node:http";
 
+const users = ["Andre"];
+
 const server = http.createServer((req, res) => {
-  return res.end("Hello, world!");
+  const { method, url } = req;
+
+  if (method === "GET" && url === "/users") {
+    return res
+      .setHeader("Content-Type", "application/json")
+      .end(JSON.stringify(users));
+  }
+
+  if (method === "POST" && url === "/users") {
+    users.push("Luana");
+
+    return res.writeHead(201).end();
+  }
+
+  return res.writeHead(404).end("Resource not found!");
 });
 
 server.listen(3333);
