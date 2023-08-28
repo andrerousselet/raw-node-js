402df181c8f77031b8e25202562edf2d20290dea
Andre Rousselet <andre098@hotmail.com>
2023-08-28 13:48:47 -0300

    feat(extract-query-params): add util function to manipulate query params from endpoint url and pass it to request object

--- src/utils/build-route-path.js
+++ src/utils/build-route-path.js
@@ -2,6 +2,6 @@
 export function buildRoutePath(path) {
   const routeParamsRegex = /:([a-zA-Z]+)/g;
   const pathParams = path.replaceAll(routeParamsRegex, "(?<$1>[a-z0-9-_]+)");
-  const pathRegex = new RegExp(`^${pathParams}`);
+  const pathRegex = new RegExp(`^${pathParams}(?<query>\\?(.*))?$`);
   return pathRegex;
 }
