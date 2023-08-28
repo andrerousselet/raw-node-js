6cadb8844c594c8567c342ac802b34290a01f74a
Andre Rousselet <andre098@hotmail.com>
2023-08-28 13:52:00 -0300

    feat(routes-and-db): add filtering to get handler through search query parameter

--- src/routes.js
+++ src/routes.js
@@ -9,7 +9,16 @@ export const routes = [
     method: "GET",
     path: buildRoutePath("/users"),
     handler: (req, res) => {
-      const users = db.select("users");
+      const { search } = req.query;
+      const users = db.select(
+        "users",
+        search
+          ? {
+              name: search,
+              email: search,
+            }
+          : null
+      );
       return res.end(JSON.stringify(users));
     },
   },
